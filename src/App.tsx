f6f976b988541a70abb352322b329e9148b786f3
Gabriel Bustos <gabrielbustosdev@gmail.com>
2025-01-19 22:23:16 -0300

    AW-8 Update App component to use UnderConstruction and add framer-motion dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import Home from "./components/Home"
-
+import UnderConstruction from "./components/UnderConstruction"
 
 function App() {
 
   return (
     <>
-      <header className="atlascorePrimario py-8 max-h-72">
-        <h1 className="uppercase text-center font-black text-4xl text-white">
-          AtlasCore header
-        </h1>
-      </header>
-      <Home />
+      <UnderConstruction />
     </>
   )
 }
